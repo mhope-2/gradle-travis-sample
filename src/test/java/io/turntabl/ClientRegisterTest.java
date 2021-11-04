4018ce63993e534b87f40d57db15e7e3fd8eeace
Michael Hope <hopemichael720@gmail.com>
2021-11-04 15:52:59 +0000

    commit

--- src/test/java/io/turntabl/ClientRegisterTest.java
+++ src/test/java/io/turntabl/ClientRegisterTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.time.Year;
 import java.util.*;
 
 import static org.junit.Assert.*;
@@ -89,7 +90,28 @@ public class ClientRegisterTest {
 
     @Test
     public void testCountClientsAtAllLevels(){
+        // create an a/c manager
+        AccountManager c2 = new AccountManager("C2 Account Manager Name");
+        AccountManager c3 = new AccountManager("C3 Account Manager Name");
 
+        List<Client> clientList = new ArrayList<>(
+                Arrays.asList(
+                        new PrivateClient(ClientType.PRIVATE, "C1", "001", Level.GOLD),
+                        new CorporateClient(ClientType.CORPORATE, "002", Level.PLATINUM,
+                                c2),
+                        new CorporateClient(ClientType.CORPORATE, "003", Level.GOLD, c3),
+                        new PrivateClient(ClientType.PRIVATE, "C4", "004", Level.PREMIUM)
+                )
+        );
+
+        ClientRegister clientRegister = new ClientRegister(clientList);
+
+        Map<Level, Long> countMap  = new HashMap<>();
+        countMap.put(Level.PLATINUM, 1L);
+        countMap.put(Level.GOLD, 2L);
+        countMap.put(Level.PREMIUM, 1L);
+
+        assertEquals(clientRegister.countClientsAtAllLevels(),countMap);
     }
 
 
